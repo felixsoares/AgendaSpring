d1c7e3602bdabe92c6020c448e0538d5845316b3
Felix Soares <felix.soares.ferreira@gmail.com>
2018-01-04 16:34:51 -0200

    build

--- src/main/java/com/felix/agenda/model/Usuario.java
+++ src/main/java/com/felix/agenda/model/Usuario.java
@@ -8,7 +8,6 @@ import javax.persistence.Transient;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Entity
@@ -24,7 +23,6 @@ public class Usuario {
 	public String nome;
 	
 	@NotNull
-	@Type(type = "org.hibernate.type.NumericBooleanType")
 	public boolean ativo;
 	
 	@NotBlank
